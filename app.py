c2ed8e5883ef06d6c873dc6bf8673b922b632ada
Данаил Боюуклиев < >
2017-03-31 01:31:41 +0300

    15

--- app.py
+++ app.py
@@ -49,7 +49,7 @@ def get_verse():
   # r = make_response(res)
   # r.headers['Content-Type'] = 'application/json'
   # return jsonify(r)
-  return "verse"
+  return result
 
 if __name__ == '__main__':
   port = int(os.getenv('PORT', 5000))
